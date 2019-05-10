53b8359024d0e967626c3f0da4459be9d7e5028e
Jose Jo Escobar <jo14343@uvg.edu.gt>
2019-05-10 03:18:21 -0600

    Update Datasets
    Al menos uno

--- src/NNet.py
+++ src/NNet.py
@@ -23,16 +23,7 @@ import random
 class NNet(object):
 
     def __init__(self, sizes):
-        """
-        En la lista 'Sizes" podemos ver el numero de neuronas en cada capa respectiva de la red.
-        Por ejemplo
-        si la lista era de [2,3,1] seria una red de tres capas, con la primer capa con 2 neuronas , 3 neuronas en la segunda capa
-        y 1 neurona de output
 
-        La red es iniciada aleatoriamente  usando la distribucion gaussiana con media 0  y varianza de 1
-        En la primer capa se asume que es la capa de entrada y por facilidad no vamos a establecer ningun sesgo
-        para las neuronas de entrada ya que unicamente en la ultima capa se calcularian los sesgos.
-        """
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
@@ -44,9 +35,7 @@ class NNet(object):
         print(len(self.biases))
 
     def feedforward(self, a):
-        """
-        Devolvemos el output de la red al ingresarlo en 'a'
-         """
+
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a)+b)
         return a
@@ -61,14 +50,7 @@ class NNet(object):
 
     def SGD(self, training_data, corrida, mini_batch_size, eta,
             test_data=None):
-        """
-        Se entrana la red neuronal con el desesnso de gradiente estocastico ya que es mas rapido su entrenamiento
-        El 'training data' es la lista de tuplas (x,y) que representa las entradas de entrenamiento
-        y el output deseado.
-        Los otros parametros no opcionales se explican solos, Si damos el 'test data' la red evaluara
-        contra los datos de prueba despues de cada corrida y proceso parcial que se muestra
-        Es util para ver el proceso pero vuelve lento un poco.
-        """
+
         if test_data: n_test = len(test_data)
         n = len(training_data)
         for j in range(corrida):
@@ -85,12 +67,7 @@ class NNet(object):
                 print("EPOCA {0} completada".format(j))
 
     def batch(self, mini_batch, eta):
-        """
-        Se actualizan los pesos y bias de la red aplicando el desenso del gradiente y el backprop
-        en una pequena porcion de los datos de aprendizaje
 
-        Basicamente es una lista de tuplas  *x,y) , y, eta --> que es la tasa de aprendizaje
-        """
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch:
@@ -103,35 +80,23 @@ class NNet(object):
                        for b, nb in zip(self.biases, nabla_b)]
 
     def backpropagation(self, x, y):
-        """
-        Devolvemos una tupla (nabla_b,nabla_v) que seria el gradiente de la funcion de costo C_x
-        nabla_b y nabla_v ---> son listas capa por capa de matrices de numpy, son donde se guardan los cambios
-        a los pesos y los biases por capa
-        """
+
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         activation = x
-        activations = [x] # Lista que almacena todas las activaciones capa por capa
-        zs = [] # Lista que almacena todos los vectores z, capa por capa
+        activations = [x] 
+        zs = [] 
 
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation)+b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
-        # BACKWARD PASS
         delta = self.cost_derivative(activations[-1], y) * \
             sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-        '''
-        Se toma en cuenta que la variable 1 es el bucle de abajo que es utilizado casi directamente 
-        a la notacion que lleva el backprop
-        1 =1 es la ultima capa de neuronas
-        1 =2 es la segunda capa y asi
-        
-        Son al final los indices negativos de las listas creadas por python
-        '''
+
 
         for l in range(2, self.num_layers):
             z = zs[-l]
@@ -142,24 +107,17 @@ class NNet(object):
         return (nabla_b, nabla_w)
 
     def evaluate(self, test_data):
-        """
-        Devolvemos el numero de inputs de prueba que las neuronas de la red produce para el resultado correcto
-        El output de la red es el indice de cualquera de las neuronas en la capa final que tiene la activacion mas alta.
-        """
+
         test_results = [(np.argmax(self.feedforward(x)), y)
                         for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
 
     def cost_derivative(self, output_activations, y):
-        """
-        Damos el vector de la derivada parcial ---> parcial C_x que es la parcial para las activaciones del output
-        """
+
         return (output_activations-y)
 
 def sigmoid(z):
-    """ La función sigmoidea. """
     return 1.0/(1.0+np.exp(-z))
 
 def sigmoid_prime(z):
-    """Derivada de la función sigmoidea."""
     return sigmoid(z)*(1-sigmoid(z))
